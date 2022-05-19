a1d40ff1d6432050f66946750659c677852b6fea
askmadden17 <maddenm3@yahoo.com>
2022-05-19 09:21:03 +0800

    first commit

--- client/src/components/pages/Meet.js
+++ client/src/components/pages/Meet.js
@@ -1,8 +1,4 @@
 import React from "react"
-import Card from "../subcomponents/Card"
-import userData from "../subcomponents/userData"
-import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
-import Match from "./meetpages/Match"
 import MeetBody from "./meetstructure/MeetBody"
 import MeetHeader from "./meetstructure/MeetHeader"
 
@@ -10,10 +6,6 @@ export default function Meet(){
 
     return(
         <div>
-            <div>
-                <MeetHeader 
-                    />
-            </div>
             <div>
                 <MeetBody />
             </div>
